0ad63e01599af158527e15d5b9e1ba22ee0eb26a
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-03-05 13:03:27 +0400

    Update AuthRepositoryImpl.kt

--- app/src/main/java/com/example/composesample/authmoduleyt/data/AuthRepositoryImpl.kt
+++ app/src/main/java/com/example/composesample/authmoduleyt/data/AuthRepositoryImpl.kt
@@ -3,8 +3,10 @@ package com.example.composesample.authmoduleyt.data
 import com.example.composesample.authmoduleyt.domain.repository.AuthRepository
 import kotlinx.coroutines.delay
 
+//executig threads with coroutines
 class AuthRepositoryImpl : AuthRepository {
 
+    //1 second simulaion of loading
     override suspend fun login(email: String, password: String): Boolean {
         delay(1000)
         return true
