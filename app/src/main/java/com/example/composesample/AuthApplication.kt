ffb5ee9f88ab259f7d047977bd04abff146be9f4
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-03-05 12:57:49 +0400

    Update AuthApplication.kt

--- app/src/main/java/com/example/composesample/AuthApplication.kt
+++ app/src/main/java/com/example/composesample/AuthApplication.kt
@@ -3,6 +3,8 @@ package com.example.composesample
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+//for dependency injection we are using hilt library
+//here his defining hilt
 @HiltAndroidApp
 class AuthApplication : Application() {
 }
