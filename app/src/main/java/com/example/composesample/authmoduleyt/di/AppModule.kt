8c14cf26043abc2f55aad4d48072c8eb9dd269fd
Tamerlan Mustafayev <tamerlan_94@mail.ru>
2023-04-02 23:01:38 +0400

    Update AppModule.kt

--- app/src/main/java/com/example/composesample/authmoduleyt/di/AppModule.kt
+++ app/src/main/java/com/example/composesample/authmoduleyt/di/AppModule.kt
@@ -9,7 +9,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
-
+//Dependency Injection
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
